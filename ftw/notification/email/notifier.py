98a21e7bfd2da2eb07e5e321a0f3a7096cb1c2c5
Victor Baumann <v.baumann@4teamwork.ch>
2009-09-04 12:23:01 +0000

    #644 error handling improved

--- ftw/notification/email/notifier.py
+++ ftw/notification/email/notifier.py
@@ -10,7 +10,12 @@ import logging
 from zope import component
 from zope.sendmail.interfaces import IMailer
 from ftw.notification.email.interfaces import IEMailRepresentation
-from ftw.notification.email import emailNotificationMessageFactory as _
+from ftw.notification.base import notification_base_factory as _
+from Products.statusmessages.interfaces import IStatusMessage
+from zope.publisher.interfaces import Retry
+from ZODB.POSException import ConflictError 
+import traceback
+import sys
 logger = logging.getLogger('izug.notification.email')
 
 class MailNotifier(BaseNotifier):
@@ -20,6 +25,7 @@ class MailNotifier(BaseNotifier):
         portal_membership = getToolByName(object_ or site, 'portal_membership')
 
         recipients = {}
+        
         for user_id in to_list:
             member = portal_membership.getMemberById(user_id)
             if member is None:
@@ -31,14 +37,17 @@ class MailNotifier(BaseNotifier):
             if email is None:
                 continue
             recipients[user_id] = (fullname, email)
+
+        if not recipients:
+            return
         
         sender = None    
         sender_id = kwargs.get('actor', '')
         sender_data = portal_membership.getMemberById(sender_id)
         if sender_data is not None:
-            sender_fullname = sender_data.getProperty('fullname', user_id)
-            if not len(fullname):
-                fullname = user_id
+            sender_fullname = sender_data.getProperty('fullname', sender_id)
+            if not len(sender_fullname):
+                sender_fullname = sender_id
             sender_email =  sender_data.getProperty('email', '')  
             sender = (sender_fullname, sender_email)      
         kwargs.update(dict(sender=sender))
@@ -46,9 +55,15 @@ class MailNotifier(BaseNotifier):
             try:
                 #XXX translate...
                 email = IEMailRepresentation(object_)('[iZug] Benachrichtigung', recipients.values(), message, **kwargs)
-            except Exception, e:
-                email = None
-            if email is not None:
                 smtp = component.getUtility(IMailer, 'plone.smtp')
                 smtp.update_settings()
                 smtp.send(email['From'], email['To'], email.as_string())
+                IStatusMessage(object_.request).addStatusMessage(_('statusmessage_notification_sent'), type='info')
+            except (ConflictError, Retry):
+                raise
+            except Exception, error:
+                exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+                logger.error('Error while sending notification')
+                traceback.print_exception(exceptionType, exceptionValue, exceptionTraceback, file=sys.stdout)
+                IStatusMessage(object_.request).addStatusMessage(_('statusmessage_notification_not_sent'), type='error')
+                
