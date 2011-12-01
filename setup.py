e0f4599c54959152659da9b9710f076a55c16b5d
Jonas Baumann <jone@jone.ch>
2011-12-01 10:03:16 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,40 +6,51 @@ maintainer = 'Jonas Baumann'
 
 tests_require = [
     'plone.app.testing',
-]
+    'plone.testing',
+    ]
 
 setup(name='ftw.notification.email',
       version=version,
-      description="Maintainer: %s" % maintainer,
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='Send edit-notifications by email.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='',
-      author='%s, 4teamworkk GmbH' % maintainer,
+
+      keywords='ftw notification email plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.notification.email/',
+      url='https://github.com/4teamwork/ftw.notification.email',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.notification'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'stoneagehtml',
-          'ftw.journal',
-          'ftw.notification.base',
 
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=[
+        'setuptools',
+        'stoneagehtml',
+        'ftw.journal',
+        'ftw.notification.base',
+        ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      entry_points="""
+      entry_points='''
       # -*- Entry points: -*-
-      """,
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
       )
